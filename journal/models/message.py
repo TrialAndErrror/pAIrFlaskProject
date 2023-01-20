da04514cba7451e86f22daa17ac27248596ae221
Trial And Errror <coding@trialanderrror.com>
2023-01-20 17:29:49 -0500

    fix water bug

--- journal/models/message.py
+++ journal/models/message.py
@@ -12,5 +12,5 @@ def parse_request_data(request_json: dict) -> Message:
     return Message(
         entry_type=request_json['entry_type'],
         amount=request_json['amount'],
-        name=request_json['name']
+        name=request_json.get('name', '')
     )
