2cb016c84ffe1629e5890dc96b5058ed25554da9
TrialAndErrror <coding@trialanderrror.com>
2023-02-11 15:07:30 -0500

    Fix message bug, add build to service

--- temperature/models/message.py
+++ temperature/models/message.py
@@ -3,14 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Message:
-    entry_type: str
     temperature: float
     humidity: float
 
 
 def parse_request_data(request_json: dict) -> Message:
     return Message(
-        entry_type=request_json['entry_type'],
         temperature=request_json.get('temperature', ''),
         humidity=request_json.get('humidity', '')
     )
